405d32d2d3dd1e729f6c700dc934bedfb908bf3b
joshua <joshk.5793@gmail.com>
2023-03-24 12:53:30 -0400

    PolyTreeNode

--- practice-for-ch-ruby-poly-tree-node-long-practice-main/lib/tree_node.rb
+++ practice-for-ch-ruby-poly-tree-node-long-practice-main/lib/tree_node.rb
@@ -1,3 +1,27 @@
 class PolyTreeNode
+    attr_reader :value, :parent, :children
+    def initialize(value)
+        @value = value
+        @parent = nil
+        @children = []
+    end
+
+    def parent=(new_parent)
+        @parent.children.delete(self) if !@parent.nil?
+        @parent = new_parent
+        if !@parent.nil?
+            @parent.children << self
+        end
+    end
+
+    def add_child(child_node)
+        child_node.parent = self
+    end
+
+    def remove_child(child_node)
+        raise 'Not a child' if !children.include?(child_node)
+        child_node.parent = nil
+    end
+
   
 end
